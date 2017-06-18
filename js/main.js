2460695ba092c83dbcd02127bcea939e23bef005
Lucas Balestero <lucasbalestero@gmail.com>
2017-06-18 12:14:16 -0400

    Exp and date added to the list

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var currentExp;
 var expHistory = document.getElementById("exp-history");
+var expList = new Array();
 
 function calcExp(){
   currentExp = document.getElementById("current-exp").value;
@@ -7,6 +8,23 @@ function calcExp(){
   console.log(currentExp);
 }
 
-function addToList(exp){
-  expHistory.innerHTML += "<li>"+exp+"</exp>";
+function addToList(currentExp){
+  var obj = {exp:currentExp, date: Date.now()};
+  expList.push(obj);
+  expHistory.innerHTML += "<li>"+currentExp+" - "+ formatDate(obj.date) +"</li>";
+  console.log(expList);
 }
+
+
+function formatDate(date){
+	var m = new Date(date);
+	var dateString = 
+	m.getUTCFullYear() + "/" +
+	("0" + (m.getUTCMonth()+1)).slice(-2) + "/" +
+	("0" + m.getUTCDate()).slice(-2) + " " +
+	("0" + m.getUTCHours()).slice(-2) + ":" +
+	("0" + m.getUTCMinutes()).slice(-2) + ":" +
+	("0" + m.getUTCSeconds()).slice(-2);
+	return dateString;
+}
+
